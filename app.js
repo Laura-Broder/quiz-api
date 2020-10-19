c53683dbb984ca7e69dd6e9b0f87a16508910045
Laura Broder <laura.kokielov@gmail.com>
2020-10-19 14:13:37 +0300

    config package.json

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const quizControl = require("./quizControl");
 
-const port = 3005;
+const port = process.env.PORT || 3005;
 const app = express();
 app.use(bodyParser.json());
 
